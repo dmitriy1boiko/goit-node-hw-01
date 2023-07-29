83f94d2f5a5d27c32199a2d8634833339a04fe31
Dmitriy Boiko <dmitriy1boiko@gmail.com>
2023-07-29 14:02:21 +0300

    finally

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # goit-node-hw-01
+action-list https://ibb.co/xH2k4N4
+action-get https://ibb.co/S0y5Nbg
+action-add https://ibb.co/d08DhpC
+action-remove https://ibb.co/gFvYtwn
---
+++ index.js
@@ -0,0 +1,40 @@
+const { program } = require("commander");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+console.log(options);
+
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.table(oneContact);
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.table(newContact);
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      return console.table(removeContact);
+
+    default:
+      return console.log("Unknown action.");
+  }
+};
+
+invokeAction(options);
